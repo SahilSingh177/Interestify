81cc9f8d1645914c41f4eac28ee8e8a239f54db5
then00bprogrammer <nikhilranjan1103@gmail.com>
2023-06-17 14:12:07 +0530

    Resolved Merge Conflicts

--- client/src/Handlers/bookmarkArticle.ts
+++ client/src/Handlers/bookmarkArticle.ts
@@ -1,13 +1,16 @@
+import { auth } from '@/firebase/clientApp'
+
 export const bookmarkArticle = async (
     isBookMarked: boolean,
     ArticleLink: string
 ) => {
+    const email = auth.currentUser?.email;
     if (!isBookMarked) {
-        await fetch(`http://127.0.0.1:5000/addBookmark?email=test@mail&link=${ArticleLink}`);
+        await fetch(`http://127.0.0.1:5000/addBookmark?email=${email}&link=${ArticleLink}`);
         console.log("SUCCESSFULLY ADDED");
         return true;
     } else {
-        await fetch(`http://127.0.0.1:5000/deleteBookmark?email=test@mail&link=${ArticleLink}`);
+        await fetch(`http://127.0.0.1:5000/deleteBookmark?email=${email}&link=${ArticleLink}`);
         console.log("DELETED SUCCESSFULLY");
         return false;
     }
