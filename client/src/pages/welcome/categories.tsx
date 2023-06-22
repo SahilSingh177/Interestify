22fdaefca3731c8dfc38b08fcb6d42b8d97a31bf
then00bprogrammer <nikhilranjan1103@gmail.com>
2023-06-23 00:43:34 +0530

    fixed categories bug

--- client/src/pages/welcome/categories.tsx
+++ client/src/pages/welcome/categories.tsx
@@ -52,7 +52,7 @@ const categories: NextPageWithLayout = () => {
       <Head>
         <title>Interestify - Select Categories</title>
       </Head>
-        <Stack bgImage='/assets/category_bg.jpg'  backgroundSize="contain" bgRepeat='repeat-y'  alignItems="center" justifyContent="center">
+        <Stack width={`calc(100vw - 12px)`} minHeight='100vh' bgImage='/assets/category_bg.jpg'  backgroundSize="contain 100% auto" bgRepeat='repeat-y'  alignItems="center" >
             <Heading marginTop="5vh" textAlign='center'>SELECT YOUR FAVOURITE CATEGORIES</Heading>
             <InputGroup width="60vw" margin="5vh 5vw 0 5vw">
                 <InputLeftElement
