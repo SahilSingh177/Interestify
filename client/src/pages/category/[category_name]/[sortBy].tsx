418a104d5f7e79fcd74470ff954fc08d8fe6d410
then00bprogrammer <lcs2021006@iiitl.ac.in>
2023-06-23 14:05:57 +0530

    unoptimizing

--- client/src/pages/category/[category_name]/[sortBy].tsx
+++ client/src/pages/category/[category_name]/[sortBy].tsx
@@ -27,48 +27,28 @@ interface Article {
   isLiked: boolean;
 }
 
-const Category = ({
-  category,
-  sortBy,
-  initialData,
-}: {
-  category: string;
-  sortBy: string;
-  initialData: Article[];
-}) => {
+const Category = () => {
   const router = useRouter();
-  const [isLoading, setIsLoading] = useState(false);
-  const [data, setData] = useState<Article[]>(initialData);
+  const category = router.query.category_name as string;
+  const sortBy = router.query.sortBy as string;
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState<Article[]>([]);
   const [hasMoreData, setHasMoreData] = useState(true);
-  const [page, setPage] = useState(2);
-
-  useEffect(() => {
-    const handleRouteChange = () => {
-      if (router.query.optionalParam !== router.asPath) {
-        console.log('route changed');
-        setData(initialData);
-        setPage(2);
-        setHasMoreData(true);
-      }
-    };
-    router.events.on('routeChangeComplete', handleRouteChange);
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange);
-    };
-  }, [router]);
-
-  let endpoint = '';
-  if (sortBy === 'best')
-    endpoint = `http://127.0.0.1:5000/getTopArticlesfor?category=${category}&page=${page}`;
-  else if (sortBy === 'recent')
-    endpoint = `http://127.0.0.1:5000/getRecentArticlesfor?category=${category}&page=${page}`;
-  else
-    endpoint = `http://127.0.0.1:5000/getHotArticlesfor?category=${category}&page=${page}`;
+  const [page, setPage] = useState(1);
 
   const fetchData = async () => {
     try {
+      let endpoint = '';
+      if (sortBy === 'best')
+        endpoint = `http://127.0.0.1:5000/getTopArticlesfor?category=${category}&page=${page}`;
+      else if (sortBy === 'recent')
+        endpoint = `http://127.0.0.1:5000/getRecentArticlesfor?category=${category}&page=${page}`;
+      else
+        endpoint = `http://127.0.0.1:5000/getHotArticlesfor?category=${category}&page=${page}`;
+
       const response = await fetch(endpoint);
       const jsonData = await response.json();
+      console.log(jsonData);
       if (jsonData.length === 0) {
         setHasMoreData(false);
         return;
@@ -81,138 +61,128 @@ const Category = ({
     }
   };
 
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    const handleRouteChange = () => {
+      if (router.query.optionalParam !== router.asPath) {
+        console.log('route changed');
+        setData([]);
+        setPage(1);
+        setHasMoreData(true);
+      }
+    };
+
+    router.events.on('routeChangeComplete', handleRouteChange);
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router]);
+
   return (
     <>
-    <Head>
+      <Head>
         <title>{category}</title>
       </Head>
-    <Flex
-      flexDirection={{ lg: 'row', md: 'column', sm: 'column' }}
-      marginTop="10vh"
-      width={`calc(100vw - 12px)`}
-      justifyContent="space-evenly"
-    >
       <Flex
-        flexDirection="column"
-        justifyContent={{ lg: 'flex-start', md: 'center' }}
-        width={{ lg: '55vw', md: `calc(80vw - 12px)` }}
-        overflowX="hidden"
+        flexDirection={{ lg: 'row', md: 'column', sm: 'column' }}
+        marginTop="10vh"
+        width={`calc(100vw - 12px)`}
+        justifyContent="space-evenly"
       >
-        <Heading size="2xl" paddingLeft={2} marginBottom={10}>
-          {category}
-        </Heading>
-        <Tabs colorScheme="green">
-          <TabList>
-            <Tab
-              onClick={() =>
-                router.push(`/category/${category}/best`)
+        <Flex
+          flexDirection="column"
+          justifyContent={{ lg: 'flex-start', md: 'center' }}
+          width={{ lg: '55vw', md: `calc(80vw - 12px)` }}
+          overflowX="hidden"
+        >
+          <Heading size="2xl" paddingLeft={2} marginBottom={10}>
+            {category}
+          </Heading>
+          <Tabs colorScheme="green">
+            <TabList>
+              <Tab onClick={() => router.push(`/category/${category}/best`)}>
+                Best
+              </Tab>
+              <Tab onClick={() => router.push(`/category/${category}/recent`)}>
+                Recent
+              </Tab>
+              <Tab onClick={() => router.push(`/category/${category}/hot`)}>
+                Hot
+              </Tab>
+            </TabList>
+          </Tabs>
+          {isLoading && (
+            <Flex
+              flexDirection="column"
+              alignItems="center"
+              justifyContent="center"
+              height="30vh"
+              width="full"
+            >
+              <Spinner
+                margin="auto"
+                thickness="4px"
+                speed="0.65s"
+                emptyColor="gray.200"
+                color="blue.500"
+                size="xl"
+              />
+            </Flex>
+          )}
+          {data.length > 0 && (
+            <InfiniteScroll
+              dataLength={data.length}
+              next={fetchData}
+              hasMore={hasMoreData}
+              loader={
+                <Flex
+                  flexDirection="column"
+                  alignItems="center"
+                  justifyContent="center"
+                  height="30vh"
+                  width="full"
+                >
+                  <Spinner
+                    margin="auto"
+                    thickness="4px"
+                    speed="0.65s"
+                    emptyColor="gray.200"
+                    color="blue.500"
+                    size="xl"
+                  />
+                </Flex>
               }
             >
-              Best
-            </Tab>
-            <Tab
-              onClick={() =>
-                router.push(`/category/${category}/recent`)
-              }
-            >
-              Recent
-            </Tab>
-            <Tab
-              onClick={() =>
-                router.push(`/category/${category}/hot`)
-              }
-            >
-              Hot
-            </Tab>
-          </TabList>
-        </Tabs>
-        {data && data.length > 0 && (
-          <InfiniteScroll
-            dataLength={data.length}
-            next={fetchData}
-            hasMore={hasMoreData}
-            loader={
-              <Flex
-                flexDirection="column"
-                alignItems="center"
-                justifyContent="center"
-                height="30vh"
-                width="full"
-              >
-                <Spinner
-                  margin="auto"
-                  thickness="4px"
-                  speed="0.65s"
-                  emptyColor="gray.200"
-                  color="blue.500"
-                  size="xl"
-                />
-              </Flex>
-            }
-          >
-            {data.map((articleInfo, id) => (
-              <ArticleCard
-                articleId={articleInfo.id}
-                Author={articleInfo.author}
-                Category={category}
-                Title={articleInfo.title}
-                Summary={articleInfo.summary}
-                ReadingTime={articleInfo.time}
-                ArticleLink={articleInfo.link}
-                Likes={articleInfo.likes}
-                key={id}
-              ></ArticleCard>
-            ))}
-          </InfiniteScroll>
-        )}
+              {data.map((articleInfo, id) => (
+                <ArticleCard
+                  articleId={articleInfo.id}
+                  Author={articleInfo.author}
+                  Category={category}
+                  Title={articleInfo.title}
+                  Summary={articleInfo.summary}
+                  ReadingTime={articleInfo.time}
+                  ArticleLink={articleInfo.link}
+                  Likes={articleInfo.likes}
+                  key={id}
+                ></ArticleCard>
+              ))}
+            </InfiniteScroll>
+          )}
+        </Flex>
+        <Divider
+          orientation="vertical"
+          borderColor="black"
+          bg="black"
+          size="5px"
+        ></Divider>
+        <Sidebar></Sidebar>
       </Flex>
-      <Divider
-        orientation="vertical"
-        borderColor="black"
-        bg="black"
-        size="5px"
-      ></Divider>
-      <Sidebar></Sidebar>
-    </Flex>
     </>
   );
 };
-
-export async function getServerSideProps({
-  query,
-}: GetServerSidePropsContext<{ category_name: string; sortBy: string }>) {
-  try {
-    
-    const { category_name, sortBy } = query;
-    console.log('HIT');
-    console.log(category_name);
-    console.log(query);
-    let endpoint = '';
-
-    if (sortBy === 'best')
-      endpoint = `http://127.0.0.1:5000/getTopArticlesfor?category=${category_name}&page=1`;
-    else if (sortBy === 'recent')
-      endpoint = `http://127.0.0.1:5000/getRecentArticlesfor?category=${category_name}&page=1`;
-    else
-      endpoint = `http://127.0.0.1:5000/getHotArticlesfor?category=${category_name}&page=1`;
-
-    const resp = await fetch(endpoint);
-    const filteredResp = await resp.json();
-    return {
-      props: {
-        initialData: filteredResp,
-        category: category_name,
-        sortBy: sortBy,
-      },
-    };
-  } catch (error) {
-    console.log(error);
-    return {
-      props: {
-        data: null,
-      },
-    };
-  }
-}
 
 export default Category;
