8ddb941090798fe1b48f5e567b1bbc79ad0097f8
then00bprogrammer <nikhilranjan1103@gmail.com>
2023-06-23 16:44:37 +0530

    Revert "unoptimizing"
    This reverts commit fdc4f998b539117deb37b0647c58cbbc5c2193bc.

--- client/src/pages/article/[article_id].tsx
+++ client/src/pages/article/[article_id].tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Heading, Spinner, Stack, VStack } from '@chakra-ui/react';
 import Article from '@/components/Articles/Article';
 import ArticleRecommendations from '@/components/Articles/ArticleRecommendations';
@@ -23,97 +23,84 @@ interface Params extends ParsedUrlQuery {
   article_id: string;
 }
 
-const ArticlePage = () => {
-  const [articleData, setArticleData] = useState<ArticleData>();
+const ArticlePage = ({ articleData }: { articleData: ArticleData }) => {
   const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      setIsLoading(true);
-      try {
-        const articleId = Router.query.article_id as string;
-        const response = await fetch(`http://127.0.0.1:5000/getArticle?article_id=${articleId}`);
-        const data = await response.json();
-        const filteredData = data.data[0];
-
-        const formattedData: ArticleData = {
-          Text: filteredData['text'],
-          Author: filteredData['author'],
-          Category: filteredData['category'],
-          Title: filteredData['title'],
-          Summary: filteredData['summary'],
-          ReadingTime: filteredData['read_time'],
-          PDFLink: filteredData['pdf_link'],
-          ArticleLink: filteredData['link'],
-          ArticleId: filteredData['id'],
-        };
-
-        setArticleData(formattedData);
-      } catch (error) {
-        console.log(error);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
-    fetchData();
-  }, []);
-
-  // if (isLoading) {
-  //   return (
-  //     <>
-  //       <Head>
-  //         <title>Articles</title>
-  //       </Head>
-  //       <Spinner
-  //         margin='auto'
-  //         thickness='4px'
-  //         speed='0.65s'
-  //         emptyColor='gray.200'
-  //         color='blue.500'
-  //         size='xl'
-  //       />
-  //     </>
-  //   );
-  // }
-
-  // if (!articleData) {
-  //   return (
-  //     <>
-  //       <Head>
-  //         <title>Articles</title>
-  //       </Head>
-  //       <VStack spacing={10}>
-  //         <Heading size="2xl">INTERNAL SERVER ERROR</Heading>
-  //         <Heading size="xl">We'll be back soon</Heading>
-  //         <iframe width="100%" src="https://giphy.com/embed/ykaNntbZ3hfsWotKmA" />
-  //       </VStack>
-  //     </>
-  //   );
-  // }
 
   return (
     <>
       <Head>
         <title>Articles</title>
       </Head>
-      {articleData && <Stack direction={["column", 'column', 'column', 'row']} width="calc(100vw - 12px)" maxWidth="100vw" overflowX='hidden'>
-        <Article
-          ArticleId={articleData.ArticleId}
-          Content={articleData.Text}
-          Author={articleData.Author}
-          Category={articleData.Category || 'Unknown'}
-          Title={articleData.Title}
-          ReadingTime={articleData.ReadingTime}
-          Summary={articleData.Summary}
-          PDFLink={articleData.PDFLink}
-          ArticleLink={articleData.ArticleLink}
-        />
+      <Stack direction={["column",'column','column','row']} width="calc(100vw - 12px)" maxWidth="100vw" overflowX='hidden'>
+        {isLoading && (
+          <Spinner
+            margin='auto'
+            thickness='4px'
+            speed='0.65s'
+            emptyColor='gray.200'
+            color='blue.500'
+            size='xl'
+          />
+        )}
+        {!isLoading && !articleData && (
+          <VStack spacing={10}>
+            <Heading size="2xl">INTERNAL SERVER ERROR</Heading>
+            <Heading size="xl">We'll be back soon</Heading>
+            <iframe width="100%" src="https://giphy.com/embed/ykaNntbZ3hfsWotKmA" />
+          </VStack>
+        )}
+        {articleData && (
+          <Article
+            ArticleId={articleData.ArticleId}
+            Content={articleData.Text}
+            Author={articleData.Author}
+            Category={articleData.Category || 'Unknown'}
+            Title={articleData.Title}
+            ReadingTime={articleData.ReadingTime}
+            Summary={articleData.Summary}
+            PDFLink={articleData.PDFLink}
+            ArticleLink={articleData.ArticleLink}
+          />
+        )}
         <ArticleRecommendations ArticleId={articleData.ArticleId} />
       </Stack>
-      }
     </>
   );
 };
+
+export async function getServerSideProps({ params }: GetServerSidePropsContext<Params>) {
+  try {
+
+    const articleId = params?.article_id;
+    const response = await fetch(`http://127.0.0.1:5000/getArticle?article_id=${articleId}`);
+    const data = await response.json();
+    const filteredData = data.data[0];
+
+    const formattedData: ArticleData = {
+      Text: filteredData['text'],
+      Author: filteredData['author'],
+      Category: filteredData['category'],
+      Title: filteredData['title'],
+      Summary: filteredData['summary'],
+      ReadingTime: filteredData['read_time'],
+      PDFLink: filteredData['pdf_link'],
+      ArticleLink: filteredData['link'],
+      ArticleId: filteredData['id'],
+    };
+
+    return {
+      props: {
+        articleData: formattedData,
+      },
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      props: {
+        articleData: null,
+      },
+    };
+  }
+}
 
 export default ArticlePage;
