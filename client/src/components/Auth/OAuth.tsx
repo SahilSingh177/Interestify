838aa7539905ce4db5cbdf1aaa6986542fe58c5c
then00bprogrammer <lcs2021006@iiitl.ac.in>
2024-05-01 10:41:08 +0530

    google and fb logo

--- client/src/components/Auth/OAuth.tsx
+++ client/src/components/Auth/OAuth.tsx
@@ -14,8 +14,8 @@ const OAuth = ({ view }: Props) => {
                 paddingTop={['5vh','5vh','5vh','20vh']} paddingBottom={['0','0','0','10vh']}
                 >
                     <Text textAlign='center' color="white" fontSize={['5xl','6xl','6xl','6xl']} marginBottom="5vh">{view === "login" ? "Log In" : "Sign Up"}</Text>
-                    <OauthButtons imageSrc='https://icons8.com/icon/17949/google' providerName='Google' />
-                    <OauthButtons imageSrc='https://icons8.com/icon/uLWV5A9vXIPu/facebook' providerName='Facebook' />
+                    <OauthButtons imageSrc='https://img.icons8.com/color/48/google-logo.png' providerName='Google' />
+                    <OauthButtons imageSrc='https://img.icons8.com/fluency/240/facebook-new.png' providerName='Facebook' />
                 </Stack>
             </Center>
         </>
